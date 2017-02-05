fa78cd8ba9261762c4774fd68a8955bd7baea562
DaisyChubb <homm0004@algonquincollege.com>
2017-02-05 15:10:44 -0500

    The Toolbar is taking shape

--- app/src/main/java/com/projectmonkeyscope/MainActivity.java
+++ app/src/main/java/com/projectmonkeyscope/MainActivity.java
@@ -1,8 +1,14 @@
 package com.projectmonkeyscope;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import static com.projectmonkeyscope.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
     /**
@@ -13,12 +19,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(activity_main);
+        //Edit toolbar (ActionBar)
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         // Let's get the MAIN_PROFILE and see if it worked!
         Profile userProfile = Profile.MAIN_PROFILE;
 
         Log.i(ACTIVITY_NAME, "In onCreate() with key: " + userProfile.toString());
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /*The 3 buttons on the right side of the toolbar*/
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override//TODO: Make Navigation Drawer
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        String btnName = null;
+        switch(itemId) {
+            case R.id.menu_settings:
+                btnName = "Settings";
+                break;
+            case R.id.menu_compass:
+                btnName = "Compass";
+                break;
+            case R.id.menu_help:
+                btnName = "Help";
+                break;
+        }
+        Toast.makeText(this, "Button " + btnName, Toast.LENGTH_SHORT).show();
+        return true;
     }
 }
