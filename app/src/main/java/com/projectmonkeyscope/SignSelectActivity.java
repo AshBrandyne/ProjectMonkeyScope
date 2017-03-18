14d39da6b56d0177706f61a0841396df5d921af6
DaisyChubb <homm0004@algonquincollege.com>
2017-03-18 16:36:21 -0400

    Added SignSelect Activity

---
+++ app/src/main/java/com/projectmonkeyscope/SignSelectActivity.java
@@ -0,0 +1,13 @@
+package com.projectmonkeyscope;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_select);
+    }
+}
