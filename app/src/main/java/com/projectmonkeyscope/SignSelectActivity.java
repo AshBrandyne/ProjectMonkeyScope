518019fe59520b63309db34ac0689ea4a952113e
DaisyChubb <homm0004@algonquincollege.com>
2017-07-02 18:18:15 -0400

    Added graphics and testing 9patch

--- app/src/main/java/com/projectmonkeyscope/SignSelectActivity.java
+++ app/src/main/java/com/projectmonkeyscope/SignSelectActivity.java
@@ -2,12 +2,24 @@ package com.projectmonkeyscope;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SignSelectActivity extends AppCompatActivity {
+    /**
+     * Declaring Buttons for each sign
+     * Will change to ImageViews after designing layout
+     */
+    Button aquarius, pisces, aries, taurus, gemini, cancer, leo;
+    Button virgo, libra, scorpio, sagittarius, capricorn;
+    Button signs[] = new Button[12];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_select);
+
+        //signs = {aquarius, }
+
     }
+
 }
